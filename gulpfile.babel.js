97839f05af7330946d04d780d70fba89bef51fc5
jonasem <jonasem@outlook.com>
2015-09-19 23:49:26 +0200

    Added lots of changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,25 +2,58 @@ import gulp from 'gulp'
 import sass from 'gulp-sass'
 import concat from 'gulp-concat'
 import babel from 'gulp-babel'
+import react from 'gulp-react'
 
 const paths = {
-	styles: ['node_modules/material-design-lite/material.min.css', 'src/**/*.scss'],
-	scripts: ['node_modules/material-design-lite/material.min.js', 'src/**/*.es', 'src/**/*.js'],
+	styles: ['src/**/*.scss'],
+	scripts: [ 
+		'src/**/*.es', 
+		'src/**/*.js'],
+	templates: 'src/**/*.jsx'
 }
 
 gulp.task('styles', () => {	
 	gulp.src(paths.styles)
 		.pipe(sass.sync().on('error', () => {}))
-		.pipe(concat('index.css'))
-		.pipe(gulp.dest('.'))
+		.pipe(concat('app.css'))
+		.pipe(gulp.dest('dist'))
 });
 
 gulp.task('scripts', () => {	
 	gulp.src(paths.scripts)
-		.pipe(babel())
-		.pipe(concat('index.js'))
-		.pipe(gulp.dest('.'))
+		.pipe(babel({modules: 'ignore'}))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('dist'))
+
+	gulp.src(paths.templates)
+		.pipe(react({harmony: false, es6module: true}))
+		.pipe(babel({modules: 'ignore'}))
+		.pipe(concat('templates.js'))
+		.pipe(gulp.dest('dist'))	
 });
 
+gulp.task('vendor', () => {	
+	let scripts = [
+		'node_modules/material-design-lite/material.min.js', 
+		'node_modules/react/dist/react.min.js']
+	let styles = [
+		'node_modules/material-design-lite/material.min.css'
+	]
 
-gulp.task('default', ['styles', 'scripts'], () => {});
+	gulp.src(scripts)
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('dist'))
+
+	gulp.src(styles)
+		.pipe(concat('vendor.css'))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['scripts', 'styles'],() => {
+	gulp.watch(paths.scripts, ['scripts'])
+	gulp.watch(paths.templates, ['scripts'])
+	gulp.watch(paths.styles, ['styles'])
+})
+
+gulp.task('build', ['styles', 'scripts', 'vendor'])
+gulp.task('default', ['build'])
---
+++ src/index.es
@@ -0,0 +1,15 @@
+export class Index {
+  constructor() {
+  	this.heading = 'Welcome to the Aurelia Navigation App!';
+  	this.firstName = 'John';
+  	this.lastName = 'Doe';	
+  }
+  
+  get fullName(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  submit(){
+    alert(`Welcome, ${this.fullName}!`);
+  }
+}
